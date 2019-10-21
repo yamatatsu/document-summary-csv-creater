616c7d8bace3f8c37c382ee6be27d823cb55701e
yamatatsu <the.kinnikuman@gmail.com>
2019-10-21 23:20:46 +0900

    test refactor

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,37 +1,37 @@
 import { toCsvRow, getTitle } from "../src/functions"
 
-test("toCsvRow", async () => {
+test("[toCsvRow]", async () => {
   const input = ['"foo"', "bar,"]
   expect(toCsvRow(input)).toBe('"""foo""","bar,"')
 })
 
-test("getTitle", async () => {
+test("[getTitle] header has '#'", async () => {
   const input = `## foo #bar`
   expect(getTitle(input)).toBe("foo #bar")
 })
 
-test("getTitle", async () => {
+test("[getTitle] header1 after header2", async () => {
   const input = `
 ## foo
 # bar`
   expect(getTitle(input)).toBe("bar")
 })
 
-test("getTitle", async () => {
+test("[getTitle] exists two header1s", async () => {
   const input = `
 # foo
 # bar`
   expect(getTitle(input)).toBe("foo")
 })
 
-test("getTitle", async () => {
+test("[getTitle] no header", async () => {
   const input = `
 foo
 bar`
   expect(getTitle(input)).toBe("")
 })
 
-test("getTitle", async () => {
+test("[getTitle] no header1", async () => {
   const input = `
 ### foo
 ## bar`
